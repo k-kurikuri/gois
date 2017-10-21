0422cdabe8c597b92ef08fca2673421cb5b6d89b
k-kurikuri <kurimixgoo@gmail.com>
2017-10-21 16:39:08 +0900

    mainからdbのロジックを分離&gormを使用するようリファクタ

--- main.go
+++ main.go
@@ -1,88 +1,63 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
-	"log"
 	"net/http"
 	"net/url"
-	"os"
 	"time"
 	"github.com/k-kurikuri/gois/slack"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	models "github.com/k-kurikuri/gois/db"
+	"strconv"
 )
 
 const (
-	CompanyNameColumn = "name"
-	RequestUrl        = "http://whois.jprs.jp"
-	SleepTime         = 10
+	companyNameColumn = "name"
+	requestUrl = "http://whois.jprs.jp"
+	sleepTime = 10
 )
 
 func main() {
 	fmt.Println("===== func main start!! =====")
 
 	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(".env file not found")
-	}
+	ifErrorNilIsPanic(err)
 
-	db := sqlOpen()
+	db := models.DbOpen()
 
 	defer db.Close()
 
-	rows := query(db, "SELECT code, name FROM m_company")
+	companies := []models.MCompany{}
+	db.Find(&companies)
 
-	columns, _ := rows.Columns()
+	for _, company := range companies {
+		fmt.Println(strconv.Itoa(company.Code) + ":" + company.Name)
 
-	values := make([]sql.RawBytes, len(columns))
-
-	scanArgs := make([]interface{}, len(values))
-	for i := range values {
-		scanArgs[i] = &values[i]
-	}
-
-	companyNames := make(map[string]string, 0)
-	for rows.Next() {
-		err := rows.Scan(scanArgs...)
-		ifErrorNilIsPanic(err)
-
-		companyNames[string(values[0])] = string(values[1])
-	}
-
-	for code, companyName := range companyNames {
 		// postパラメータ生成
-		postParams := makePostParams(companyName)
+		postParams := makePostParams(company.Name)
 
 		// postリクエスト
-		resp, _ := http.PostForm(RequestUrl, postParams)
+		resp, _ := http.PostForm(requestUrl, postParams)
+		defer resp.Body.Close()
+
 		// レスポンスをgo-queryで解析
 		doc, err := goquery.NewDocumentFromResponse(resp)
 		ifErrorNilIsPanic(err)
 
-		defer resp.Body.Close()
-
-		fmt.Println(code + ":" + companyName)
-
 		// preタグ内のaタグ内テキストを出力
 		doc.Find("pre").Each(func(_ int, s *goquery.Selection) {
 			s.Find("a").Each(func(_ int, aSec *goquery.Selection) {
-				db := sqlOpen()
-				rows := query(db, "SELECT domain FROM domain_list WHERE m_company_code = "+code)
-				columns, _ := rows.Columns()
-				values := make([]sql.RawBytes, len(columns))
-				scanArgs := make([]interface{}, len(values))
-				for i := range values {
-					scanArgs[i] = &values[i]
-				}
+				domainLists := []models.DomainList{};
+				db.Find(&domainLists, "m_company_code=?", company.Code)
 
 				var whoisDomain string = aSec.Text()
 
 				isExist := false
-				for rows.Next() {
-					rows.Scan(scanArgs...)
-					if whoisDomain == string(values[0]) {
+				for _, domainList := range domainLists {
+					if whoisDomain == domainList.Domain {
 						isExist = true
 					}
 				}
@@ -90,36 +65,24 @@ func main() {
 				// 新しいドメインが存在した
 				if !isExist {
 					fmt.Println("new Domain -> " + whoisDomain)
-					_, err = db.Exec(
-						"INSERT INTO domain_list (m_company_code, domain, reporting_date) VALUES (?, ?, ?)",
-						code,
-						whoisDomain,
-						"2017-09-09 00:00:00",
-					)
-					ifErrorNilIsPanic(err)
 
-					slack.IncomingWebHook(code, companyName, whoisDomain)
+					domain := models.DomainList{}
+					domain.MCompanyCode = company.Code
+					domain.Domain = whoisDomain
+					domain.ReportingDate = time.Now()
+					db.Create(&domain)
+
+					slack.IncomingWebHook(strconv.Itoa(company.Code), company.Name, whoisDomain)
 				}
-
-				defer db.Close()
 			})
 		})
+
 		fmt.Println("==========================================")
 		fmt.Println()
 
 		// 同一IP制限に引っかかるのでN秒待機
 		sleep()
 	}
-}
-
-// mysql open
-func sqlOpen() *sql.DB {
-	dataSource := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@/" + os.Getenv("DB_DATABASE")
-
-	db, err := sql.Open("mysql", dataSource)
-	ifErrorNilIsPanic(err)
-
-	return db
 }
 
 // if err != nil is panic!
@@ -140,14 +103,5 @@ func makePostParams(value string) url.Values {
 
 // time.Sleep wrapper function
 func sleep() {
-	time.Sleep(SleepTime * time.Second)
+	time.Sleep(sleepTime * time.Second)
 }
-
-// db.Query wrapper function
-func query(db *sql.DB, sql string) *sql.Rows {
-	rows, err := db.Query(sql)
-	ifErrorNilIsPanic(err)
-
-	return rows
-}
-
