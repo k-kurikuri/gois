9b3d608d5c26b2c8b053d4d9a018c39ed0b4f46d
k-kurikuri <kurimixgoo@gmail.com>
2017-10-21 16:38:41 +0900

    gorm処理を別パッケージにまとめた

---
+++ db/models.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"time"
+	"os"
+	"github.com/joho/godotenv"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type MCompany struct {
+	Code int `gorm:"primary_key"`
+	MTokyoScId int
+	Name string
+	WhoisRegistName string
+	WebSite string
+	CreatedAt time.Time
+}
+
+type DomainList struct {
+	MCompanyCode int
+	Domain string
+	ReportingDate time.Time
+	CreatedAt time.Time
+}
+
+func init() {
+	godotenv.Load()
+}
+
+func DbOpen() *gorm.DB {
+	rdbms := "mysql";
+	dbName := os.Getenv("DB_DATABASE");
+	user := os.Getenv("DB_USER");
+	pass := os.Getenv("DB_PASSWORD");
+
+	db, err := gorm.Open(rdbms,  user+":"+pass+"@/"+dbName+"?parseTime=true&loc=Local")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.SingularTable(true)
+
+	return db
+}
